c0b1b3024770f5e362916d5bc5e8f8f1ddc8bbcb
Jorge Andrade <jorgeluiz112233@gmail.com>
2022-06-27 23:41:45 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import { Wheel } from 'react-custom-roulette'
+
+import Llama from './assets/llama.png'
+
+import { Container } from './styles'
+
+function App() {
+  const [spin, setSpin] = useState<boolean>(false)
+
+  const data = [
+    { option: 'Condomínio Vários Djanho' },
+    { option: 'Circuito Muito Djanho' },
+    { option: 'Lenharinha Haja Djanho' },
+    { option: 'Estação do Djanho' },
+    { option: 'Bosque do Djanho' },
+    { option: 'Caverna do Djanho' },
+    { option: 'Toada do Djanho' },
+    { option: 'Bobinhas do Djanho' },
+    { option: 'Os Djanhosies' },
+    { option: 'Djanhuário' },
+    { option: 'O Djanhim Diário' },
+    { option: 'Minas Djaneiras' },
+    { option: 'Caverna do Djanho' },
+    { option: 'Djanho Cremoso' }
+  ]
+
+  return (
+    <div className="App">
+      <Container>
+        <div className="parent-container">
+          <Wheel
+            mustStartSpinning={spin}
+            prizeNumber={Math.floor(Math.random() * (14 - 0 + 1) + 0)}
+            data={data}
+            spinDuration={Math.random() + 0.25}
+            onStopSpinning={() => {
+              setSpin(false)
+            }}
+            fontSize={13}
+            backgroundColors={[
+              '#9400D3',
+              '#4B0082',
+              '#0000FF',
+              '#00FF00',
+              '#FFFF00',
+              '#FF7F00',
+              '#FF0000'
+            ]}
+          />
+          <button
+            onClick={() => {
+              setSpin(true)
+            }}
+          >
+            <img src={Llama} alt="Girar" />
+          </button>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default App
---
+++ src/styles.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  width: 100%;
+  z-index: 100;
+
+  .parent-container {
+    width: 600px;
+    height: 600px;
+
+    > div {
+      height: 100%;
+      width: 100%;
+      max-height: unset;
+      max-width: unset;
+    }
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    > button {
+      cursor: pointer;
+      width: fit-content;
+      height: fit-content;
+      border-radius: 100%;
+      outline: none;
+      border: none;
+      background-color: transparent;
+      position: absolute;
+      left: 0;
+      right: 0;
+      margin-left: auto;
+      margin-right: auto;
+      z-index: 999;
+
+      :hover {
+        filter: brightness(110%);
+        animation: rotate-scale-up 0.65s infinite;
+        @keyframes rotate-scale-up {
+          0% {
+            transform: scale(1) rotateZ(0);
+          }
+          50% {
+            transform: scale(2) rotateZ(180deg);
+          }
+          100% {
+            transform: scale(1) rotateZ(360deg);
+          }
+        }
+      }
+
+      > img {
+        width: 100px;
+        height: 100px;
+      }
+    }
+  }
+`
+
+export const WheelContainer = styled.div``
